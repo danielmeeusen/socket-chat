42a09119445526cef80b140399713a316d7c56b4
Daniel Meeusen <danmeeusen@gmail.com>
2018-09-25 02:45:44 -0700

    s

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var socket = require("socket.io");
+
+// app setup
+var app = express();
+
+console.log(process.env.NODE_ENV);
+
+var server = app.listen(4000, function() {
+  console.log("listening on port 4000...");
+});
+
+// static files
+app.use(express.static("public"));
+
+// socket setup
+var io = socket(server);
+
+io.on("connection", socket => {
+  console.log("made socket connection", socket.id);
+});
