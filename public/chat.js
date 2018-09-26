b2bd260e62b5d15319ffb809d438cb1a4c56d7b6
Daniel Meeusen <danmeeusen@gmail.com>
2018-09-25 22:48:43 -0700

    4 emitting messages

--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,23 @@
 // make connection
 var socket = io.connect("http://localhost:4000");
+
+// Query DOM
+
+var message = document.getElementById('message');
+    handle = document.getElementById('handle'),
+    btn = document.getElementById('send'),
+    output = document.getElementById('output');
+
+// emit events
+
+btn.addEventListener('click', () => {
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    })
+});
+
+// listen for events
+socket.on('chat', data => {
+    output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
+});
